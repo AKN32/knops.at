59f74122728b409720c7f280b3a0e3e932983d79
Andreas Knops <andreas@knops.at>
2021-07-01 21:23:32 +0200

    initial check in

---
+++ pages/index.vue
@@ -0,0 +1,36 @@
+<template>
+
+<div class="container">
+  <div class="center">
+     <img src="/wappen.png" />
+  </div>
+</div>
+
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
+
+<style>
+
+.container {
+display: flex;
+  justify-content: center;
+
+}
+
+.center {
+ margin: 0;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  -ms-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+
+ 
+}
+
+</style>
